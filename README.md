65d8e01013858741449d4e645fcbbcbea5e07757
Elvis Luciano <elvis.gs@gmail.com>
2016-09-21 11:48:05 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# SPED Checker
+
+Verifica a estrutura de arquivos SPED Fiscal/Contribuições:
+- Bloco ou registro não suportado
+- Linha com número de campos diferente do esperado
+
+## Requisitos
+
+- Node >= 0.12
+
+## Instalação
+
+```shell
+$ npm install -g sped-checker
+```
+
+## Uso
+
+```shell
+$ sped-checker <caminho_arquivo_sped>
+```
---
+++ check.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const spedFile = process.argv[2];
+
+if (!spedFile) {
+    console.log('Uso: sped-checker <caminho_arquivo_sped>');
+    process.exit(1);
+}
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream(spedFile)
+});
+
+let lineNumber = 0;
+let fieldCounts;
+let hasErrors = false;
+
+lineReader.on('line', line => {
+    lineNumber++;
+
+    line = line.replace(/^\|/, '').replace(/\|$/, '');
+    const values = line.split('|');
+
+    if (lineNumber === 1) {
+        const layout = /^\d{8}$/.test(values[3]) ? 'fiscal' : 'contrib';
+        fieldCounts = require(`./${layout}`);
+    }
+
+    const reg = values[0];
+
+    if (!fieldCounts[reg]) {
+        console.log(`Linha ${lineNumber}: bloco ${reg[0]} ou registro ${reg} nao suportado`);
+        hasErrors = true;
+        return;
+    }
+
+    const expected = fieldCounts[reg];
+    const found = values.length;
+
+    if (found !== expected) {
+        console.log(`Linha ${lineNumber}: esperado(s) ${expected}, encontrado(s) ${found} campo(s)`);
+        hasErrors = true;
+    }
+});
+
+lineReader.on('close', () => {
+    if (!hasErrors) {
+        console.log('Nao foram encontrados erros')
+    }
+
+    console.log(`\n${lineNumber} linhas lidas`);
+});
