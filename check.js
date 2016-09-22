bb6516273263f875209a67aeb967749ce40b7a24
Elvis Luciano <elvis.gs@gmail.com>
2016-09-22 17:04:47 -0300

    Code quality

--- check.js
+++ check.js
@@ -5,12 +5,12 @@
 const spedFile = process.argv[2];
 
 if (!spedFile) {
-    console.log('Uso: sped-checker <caminho_arquivo_sped>');
-    process.exit(1);
+  console.log('Uso: sped-checker <caminho_arquivo_sped>');
+  process.exit(1);
 }
 
 const lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream(spedFile)
+  input: require('fs').createReadStream(spedFile)
 });
 
 let lineNumber = 0;
@@ -18,37 +18,37 @@ let fieldCounts;
 let hasErrors = false;
 
 lineReader.on('line', line => {
-    lineNumber++;
+  lineNumber++;
 
-    line = line.replace(/^\|/, '').replace(/\|$/, '');
-    const values = line.split('|');
+  line = line.replace(/^\|/, '').replace(/\|$/, '');
+  const values = line.split('|');
 
-    if (lineNumber === 1) {
-        const layout = /^\d{8}$/.test(values[3]) ? 'fiscal' : 'contrib';
-        fieldCounts = require(`./${layout}`);
-    }
+  if (lineNumber === 1) {
+    const layout = /^\d{8}$/.test(values[3]) ? 'fiscal' : 'contrib';
+    fieldCounts = require(`./${layout}`);
+  }
 
-    const reg = values[0];
+  const reg = values[0];
 
-    if (!fieldCounts[reg]) {
-        console.log(`Linha ${lineNumber}: bloco ${reg[0]} ou registro ${reg} nao suportado`);
-        hasErrors = true;
-        return;
-    }
+  if (!fieldCounts[reg]) {
+    console.log(`Linha ${lineNumber}: bloco ${reg[0]} ou registro ${reg} nao suportado`);
+    hasErrors = true;
+    return;
+  }
 
-    const expected = fieldCounts[reg];
-    const found = values.length;
+  const expected = fieldCounts[reg];
+  const found = values.length;
 
-    if (found !== expected) {
-        console.log(`Linha ${lineNumber}: esperado(s) ${expected}, encontrado(s) ${found} campo(s)`);
-        hasErrors = true;
-    }
+  if (found !== expected) {
+    console.log(`Linha ${lineNumber}: esperado(s) ${expected}, encontrado(s) ${found} campo(s)`);
+    hasErrors = true;
+  }
 });
 
 lineReader.on('close', () => {
-    if (!hasErrors) {
-        console.log('Nao foram encontrados erros')
-    }
+  if (!hasErrors) {
+    console.log('Nao foram encontrados erros');
+  }
 
-    console.log(`\n${lineNumber} linhas lidas`);
+  console.log(`\n${lineNumber} linhas lidas`);
 });
